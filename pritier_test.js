637779c640031860d56c2f25f18cfc81a2557051
Promatheusz <promatheus.op@gmail.com>
2025-07-11 10:36:19 +0200

    first commit

---
+++ pritier_test.js
@@ -0,0 +1,7 @@
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+const user = { firstName: "Mario", lastName: "Rossi", age: 25 };
+const sayGoodbye = (name) => {
+  return "Goodbye, " + name;
+};
